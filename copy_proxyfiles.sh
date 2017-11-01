d7f750670b4c7d9b5ace2e5ee884d23a430e8971
C. R. Oldham <cro@ncbt.org>
2017-11-01 08:19:30 -0600

    Make copy script more verbose.

--- copy_proxyfiles.sh
+++ copy_proxyfiles.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd SaltStack_Network_Edge_Device
+echo "Copying beacons..."
 cp beacon/* $VE/salt/salt/beacons
+echo "Copying execution modules..."
 cp modules/* $VE/salt/salt/modules
+echo "Copying proxymodules..."
 cp proxy/* $VE/salt/salt/proxy
+echo "Copying Lua scripts..."
 cp *.lua /srv/salt/files
+echo "Done!"
 cd $VE
